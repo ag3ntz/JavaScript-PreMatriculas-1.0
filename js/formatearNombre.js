6fbbd5bf076504b0675dcf925bda485ab804b92b
Pablo Z (Ag3ntZ) <pczamorano.f@gmail.com>
2025-08-27 11:17:59 -0400

    Modificacion CSS: Formulario

--- js/formatearNombre.js
+++ js/formatearNombre.js
@@ -1,7 +1,15 @@
+// document.addEventListener("input", function (e) {
+//     if (["nombres", "apellidoPaterno", "apellidoMaterno", "nombreApoderado"].includes(e.target.id)) {
+//         e.target.value = e.target.value
+//             .toLowerCase()
+//             .replace(/\b\w/g, l => l.toUpperCase());
+//     }
+// });
+
 document.addEventListener("input", function (e) {
-    if (["nombres", "apellidoPaterno", "apellidoMaterno", "nombreApoderado"].includes(e.target.id)) {
+    if (["nombres", "apellidoPaterno", "apellidoMaterno", "nombreApoderado", "nombrePadre", "nombreMadre", "nombreSuplente"].includes(e.target.id)) {
         e.target.value = e.target.value
             .toLowerCase()
-            .replace(/\b\w/g, l => l.toUpperCase());
+            .replace(/(^\p{L}|\s\p{L})/gu, l => l.toUpperCase());
     }
 });
