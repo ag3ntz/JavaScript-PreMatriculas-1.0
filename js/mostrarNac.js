e4453b5508073c817fcb6d72064e6ca64ef20f99
Pablo Z (Ag3ntZ) <pczamorano.f@gmail.com>
2025-11-19 15:57:39 -0300

    Correcion nacionalidad(otro)

--- js/mostrarNac.js
+++
@@ -1,4 +0,0 @@
-function mostrarNac(select) {
-    const container = document.getElementById("otraNacionalidadContainer");
-    container.style.display = (select.value === "Otro") ? "block" : "none";
-}
